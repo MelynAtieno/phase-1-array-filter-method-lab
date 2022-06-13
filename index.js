315a1c33358f6375c257ec2a5d969c3b8ba52545
Melyn Atieno Were <melynatieno@gmail.com>
2022-06-13 15:55:01 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter(
+        (match) => match.toLowerCase() === name.toLowerCase()
+    )
+
+}
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter(
+        (match) => match.toLowerCase().indexOf(string.toLowerCase()) === 0
+    )
+}
+
+
+function matchName(drivers, hometown){
+    return drivers.filter((record) => record.name === hometown)
+}
+
