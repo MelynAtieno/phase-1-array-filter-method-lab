436ddc012cccf535732d128351a5ac7479c80266
Melyn Atieno Were <melynatieno@gmail.com>
2022-06-13 15:53:50 +0300

    commit tree 546b223fdc7d646e33d66c18ea59a5962cefb4d6

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter(
+        (match) => match.toLowerCase() === name.toLowerCase()
+    )
+
+}
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter(
+        (match) => match.toLowerCase().indexOf(string.toLowerCase()) === 0
+    )
+}
+
+
+function matchName(drivers, hometown){
+    return drivers.filter((record) => record.name === hometown)
+}
+
+// function findMatching(source, sought) {
+//     return source.filter(
+//       (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
+//     );
+//   }
+  
+//   function fuzzyMatch(source, testString) {
+//     return source.filter(
+//       (possibleMatch) =>
+//         possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+//     );
+//   }
+  
+//   function matchName(source, soughtName) {
+//     return source.filter((record) => record.name === soughtName);
+//   }
